463cccb5256c69a8e2146740ac161e31ff7fdbb5
Jackson Hughes <jxhughes@protonmail.com>
2026-08-20 23:25:38 +0100

    Refactor deprecated syntax (#66)

--- infra/terraform/aws/state-setup/state-backend.tf
+++ infra/terraform/aws/state-setup/state-backend.tf
@@ -4,21 +4,26 @@ provider "aws" {
 
 resource "aws_s3_bucket" "state" {
   bucket = "jhughes-tf-states"
-  acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 
   lifecycle {
     prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "state" {
+  bucket = aws_s3_bucket.state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "state" {
+  bucket = aws_s3_bucket.state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
